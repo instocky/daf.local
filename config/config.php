2926fdf60f4a1556bc68801693eb86ca417ac21e
instocky <instocky@gmail.com>
2016-07-23 12:59:24 +0300

    lesson 7/74 init Smarty finished

--- config/config.php
+++ config/config.php
@@ -2,3 +2,26 @@
 // константы обращения к контроллерам
 define('PathPrefix', '../controllers/');
 define('PathPostfix', 'Controller.php');
+
+// Используемый шаблон
+$template = 'default';
+
+// Пути к файлам шаблонов Smarty (*.tpl)
+define ('TemplatePrefix', "../views/{$template}/");
+define ('TemplatePostfix', ".tpl");
+
+// Пути к файлам в вебпространстве
+define ('TemplateWebPath', "/template/{$template}/");
+
+//> Инициализация шаблона Smarty
+// получаем полный путь к файлу Smarty.class.php
+require('../librery/Smarty/libs/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir(TemplatePrefix);
+$smarty->setCompileDir('../tmp/templates_c');
+$smarty->setCacheDir('../tmp/cache');
+$smarty->setConfigDir('../librery/Smarty/configs');
+
+$smarty->assign('templateWebPath', TemplateWebPath); // Объявление переменной Smarty templateWebPath и передача значения константы TemplateWebPath
+//<
